749a0709b73c9621c71f7b940c20b6de0252dc19
Simone <sdiricco.91@gmail.com>
2023-07-18 23:15:53 +0200

    add more menu items

--- electron/main/mainMenu.ts
+++ electron/main/mainMenu.ts
@@ -83,7 +83,59 @@ export function create(win, onClickItem) {
           role: "quit",
         },
       ],
-    }
+    },
+      // { role: 'editMenu' }
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      { role: 'delete' },
+      { type: 'separator' },
+      { role: 'selectAll' }
+    
+    ]
+  },
+  // { role: 'viewMenu' }
+  {
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forceReload' },
+      { role: 'toggleDevTools' },
+      { type: 'separator' },
+      { role: 'resetZoom' },
+      { role: 'zoomIn' },
+      { role: 'zoomOut' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' }
+    ]
+  },
+  // { role: 'windowMenu' }
+  {
+    label: 'Window',
+    submenu: [
+      { role: 'minimize' },
+      { role: 'zoom' },
+      { role: 'close' }
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click: async () => {
+          const { shell } = require('electron')
+          await shell.openExternal('https://github.com/sdiricco/vue3-electron-codemirror6')
+        }
+      }
+    ]
+  }
   ];
   template = R.clone(__template);
   defaultTemplate = R.clone(__template);
